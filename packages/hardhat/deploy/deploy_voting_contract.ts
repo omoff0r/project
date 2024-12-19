b2c0d6d1897251cf8cd36aa3076f2780a1dd3191
omoff0r <153056545+omoff0r@users.noreply.github.com>
2024-12-19 15:32:07 +0500

    Add files via upload

---
+++ packages/hardhat/deploy/deploy_voting_contract.ts
@@ -0,0 +1,27 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { VotingContract } from "../typechain-types";
+
+const deployContract: DeployFunction = async (env: HardhatRuntimeEnvironment) => {
+  // Получаем аккаунт для развертывания
+  const { deployer } = await env.getNamedAccounts();
+  
+  const { deploy } = env.deployments;
+
+  // Выполняем развертывание контракта VotingContract
+  await deploy("VotingContract", {
+    from: deployer, 
+    args: [],      
+    log: true,      
+  });
+
+  // Инициализация экземпляра контракта после развертывания
+  const contractInstance = await env.ethers.getContract<VotingContract>("VotingContract", deployer);
+
+};
+
+// Экспортируем функцию для использования в командных скриптах Hardhat
+export default deployContract;
+
+// Добавляем теги для фильтрации скриптов по меткам
+deployContract.tags = ["VotingContract"];
